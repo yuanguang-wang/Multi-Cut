82ebe490d002124553fc6d94accc52c822a5eb3c
yuanguang-wang <yuanguang.m.wang@outlook.com>
2022-04-20 23:46:20 +0800

    T.20220420.2346

--- MultiSlicerCommand.cs
+++ MultiSlicerCommand.cs
@@ -1,67 +1,49 @@
 ﻿using Rhino;
-using Rhino.Commands;
-using Rhino.Geometry;
-using Rhino.Input;
-using Rhino.Input.Custom;
-using System;
 using System.Collections.Generic;
 
 namespace MultiSlicer
 {
-    public class MultiSlicerCommand : Command
+    public class MultiSlicerCommand : Rhino.Commands.Command
     {
+        #region ATTR
         public MultiSlicerCommand()
         {
-            // Rhino only creates one instance of each command class defined in a
-            // plug-in, so it is safe to store a refence in a static property.
             Instance = this;
         }
 
-        ///<summary>The only instance of this command.</summary>
         public static MultiSlicerCommand Instance { get; private set; }
 
-        ///<returns>The command name as it appears on the Rhino command line.</returns>
-        public override string EnglishName => "MultiSlicerCommand";
+        public override string EnglishName => "MultiSlicer";
+        #endregion
 
-        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        protected override Rhino.Commands.Result RunCommand(RhinoDoc doc, Rhino.Commands.RunMode mode)
         {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
-
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint())
+            Rhino.Input.Custom.GetObject getObject = new Rhino.Input.Custom.GetObject
             {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt0 = getPointAction.Point();
+                GeometryFilter = Rhino.DocObjects.ObjectType.EdgeFilter
+            };
+            getObject.SetCommandPrompt("Pick a edge to start");
+            getObject.Get();
+            Rhino.DocObjects.ObjRef objRef = getObject.Object(0);
+            Rhino.Geometry.BrepEdge edge = objRef.Edge();
+            Rhino.Geometry.Brep brep = objRef.Brep();
+
+            if (brep == null | edge == null)
+            {
+                RhinoApp.WriteLine("Brep is invalid");
+                return Rhino.Commands.Result.Failure;
             }
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
-            {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DynamicDraw +=
-                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
-            }
+            Rhino.Input.Custom.GetPoint getPoint = new Rhino.Input.Custom.GetPoint();
+            getPoint.DynamicDraw += null;
 
-            doc.Objects.AddLine(pt0, pt1);
+            doc.Objects.Add(brep);
+
+
             doc.Views.Redraw();
             RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
 
-            // ---
-            return Result.Success;
+            return Rhino.Commands.Result.Success;
         }
     }
 }
