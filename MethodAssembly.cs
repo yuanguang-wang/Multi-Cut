4f607fbf2d03428b10fc1cf57926261deb440444
YUANGUANG WANG <yuanguang.m.wang@outlook.com>
2022-05-18 00:36:31 +0800

    T.20220518.0036

--- MethodAssembly.cs
+++ MethodAssembly.cs
@@ -17,7 +17,10 @@ namespace MultiCut
         public Rhino.Geometry.Brep BrepSource { get; set; }
         public List<Rhino.Geometry.Point3d> Pts_List { get; set; }
         public Rhino.Geometry.Collections.BrepEdgeList BEdges_List { get; set; }
-    
+        public Rhino.RhinoDoc CurrentDoc { get; set; }
+        public Rhino.Geometry.BrepEdge BEdgeInitiatedTarget { get; set; }
+        public Rhino.Geometry.Plane PlaneCutter { get; set; }
+
         #endregion
 
         public bool ObjectCollecter(string commandPrompt)
@@ -47,6 +50,87 @@ namespace MultiCut
             
         }
 
+        public bool PointsCollector()
+        {
+            Rhino.Input.Custom.GetPoint gp = new Rhino.Input.Custom.GetPoint();
+            gp.Get();
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Curve: Intersected</returns>
+        public bool CutterPlane()
+        {
+            ;
+
+            int currentPtsNumber = this.Pts_List.Count();
+
+            if (currentPtsNumber == 0)
+            {
+                return false;
+            }
+            else if (currentPtsNumber == 1)
+            {
+                Rhino.Geometry.Vector3d axis;
+                foreach (Rhino.Geometry.BrepEdge bEdge in BEdges_List)
+                {
+                    bEdge.ClosestPoint(this.Pts_List[0], out double t);
+                    Rhino.Geometry.Point3d ptProjected = bEdge.PointAt(t);
+                    // bEdgeTarget Finder //
+                    if (ptProjected.DistanceTo(this.Pts_List[0]) < this.CurrentDoc.ModelAbsoluteTolerance)
+                    {
+                        this.BEdgeInitiatedTarget = bEdge;
+                        break;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                this.BEdgeInitiatedTarget.ClosestPoint(this.Pts_List[0], out double p);
+                axis = this.BEdgeInitiatedTarget.TangentAt(p);
+                this.PlaneCutter = new Rhino.Geometry.Plane(this.Pts_List[0], axis);
+            }
+            else if (currentPtsNumber == 2)
+            {
+
+                
+            }
+            else if (currentPtsNumber == 3)
+            {
+                // Placeholder
+            }
+            else
+            {
+                // Placeholder
+            }
+
+            Rhino.Geometry.Intersect.Intersection.BrepPlane(this.BrepSource,
+                                                            this.PlaneCutter,
+                                                            this.CurrentDoc.ModelAbsoluteTolerance,
+                                                            out Rhino.Geometry.Curve[] intersecrtionCrvs,
+                                                            out Rhino.Geometry.Point3d[] intersectionPts);
+            this.CutterCrvs = intersecrtionCrvs;
+            return true;
+        }
+    }
+
+    static class EventModerator
+    {
+        static public void CutByOnePtEventMod(object s, Rhino.Input.Custom.GetPointDrawEventArgs e)
+        {
+            MultiCut.Core core = new Core();
+            core.Pts_List = new List<Rhino.Geometry.Point3d>();
+            core.Pts_List.Add(e.CurrentPoint);
+            core.CutterPlane();
+            foreach (Rhino.Geometry.Curve crv in core.CutterCrvs)
+            {
+                e.Display.DrawCurve(crv, System.Drawing.Color.Blue, 4);
+            }
+            
+        }
     }
 
     static class Failsafe
--- MultiCutCommand.cs
+++ MultiCutCommand.cs
@@ -25,45 +25,15 @@ namespace MultiCut
 
             Rhino.Input.Custom.GetPoint getFirstPoint = new Rhino.Input.Custom.GetPoint();
             getFirstPoint.SetCommandPrompt("Pick the first point");
-            getFirstPoint.DynamicDraw += null;
-            //getFirstPoint.Tag = obj;
+
+            getFirstPoint.DynamicDraw += MultiCut.EventModerator.CutByOnePtEventMod;
+
             getFirstPoint.Get();
 
             if (getFirstPoint.CommandResult() != Rhino.Commands.Result.Success)
             {
                 return getFirstPoint.CommandResult();
             }  
-            
-            Rhino.Geometry.Point3d firstPoint = getFirstPoint.Point();
-            Rhino.Geometry.BrepEdge startEdge;
-            Rhino.Geometry.Vector3d planeZAxis;
-            Rhino.Geometry.Plane plane = doc.Views.ActiveView.ActiveViewport.ConstructionPlane();
-
-            bool astralstep = true;
-            foreach (Rhino.Geometry.BrepEdge bEdge in core.BEdges_List)
-            {
-                bEdge.ClosestPoint(firstPoint, out double t);
-                Rhino.Geometry.Point3d projectedPoint = bEdge.PointAt(t);
-                double distance = projectedPoint.DistanceTo(firstPoint);
-                if (distance <= doc.ModelAbsoluteTolerance)
-                {
-                    astralstep = false;
-                    startEdge = bEdge;
-                    planeZAxis = bEdge.TangentAt(t);
-                    plane = new Rhino.Geometry.Plane(projectedPoint, planeZAxis);
-                    break;
-                }
-            }
-
-            if (astralstep)
-            {
-                RhinoApp.WriteLine("[Placeholder] astral step");
-            }
-            else
-            {
-                RhinoApp.WriteLine("on the edge");                
-            }
-
 
             doc.Views.Redraw();
             RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
